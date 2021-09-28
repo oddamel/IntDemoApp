fe3b7e08a65eed02e3958db24be1574c1f03a46c
oddamel <meling@smallscreenstudio.com>
2021-09-28 21:31:03 +0200

    start 4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello my new brave World!')
+  res.send('Hello my new brave braves World!')
 })
 
 app.listen(port, () => {
